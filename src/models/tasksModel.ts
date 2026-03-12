9aafdc0fdacb87dc43468d1ed2f931496bd969b8
Stephen <stephensouness@outlook.com>
2026-03-12 17:54:16 +0000

    feat: add CRUD models for tasks

--- src/models/tasksModel.ts
+++ src/models/tasksModel.ts
@@ -10,4 +10,38 @@ async function createTaskModel(title: string, description: string, userId: numbe
   return result.rows[0];
 }
 
-export default { createTaskModel };
+async function getTasksByUserModel(userId: number): Promise<Task[] | undefined> {
+  const result = await pool.query(`SELECT * FROM tasks WHERE user_id = $1`, [userId]);
+  return result.rows;
+}
+
+async function getTaskByUserModel(userId: number, taskId: number): Promise<Task | undefined> {
+  const result = await pool.query(`SELECT * FROM tasks WHERE user_id = $1 AND id = $2`, [userId, taskId]);
+  return result.rows[0];
+}
+
+async function updateTaskByUserModel(
+  title: string,
+  description: string,
+  userId: number,
+  taskId: number,
+): Promise<Task | undefined> {
+  const result = await pool.query(
+    `UPDATE tasks SET title = $1, description = $2 WHERE user_id = $3 AND id = $4 RETURNING *`,
+    [title, description, userId, taskId],
+  );
+  return result.rows[0];
+}
+
+async function deleteTaskByUserModel(userId: number, taskId: number): Promise<Task | undefined> {
+  const result = await pool.query(`DELETE FROM tasks WHERE user_id = $1 AND id = $2 RETURNING *`, [userId, taskId]);
+  return result.rows[0];
+}
+
+export default {
+  createTaskModel,
+  getTasksByUserModel,
+  getTaskByUserModel,
+  updateTaskByUserModel,
+  deleteTaskByUserModel,
+};
