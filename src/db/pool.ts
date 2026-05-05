e6656c42058f72256fac468163fd0790814f1e1a
Stephen <stephensouness@outlook.com>
2026-05-05 15:56:48 +0100

    Update database connection to use DATABASE_URL

--- src/db/pool.ts
+++ src/db/pool.ts
@@ -4,12 +4,21 @@ dotenv.config();
 import pkg from "pg";
 const { Pool } = pkg;
 
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
-});
+let pool;
+
+if (process.env.DATABASE_URL) {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+  });
+} else {
+  pool = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: Number(process.env.DB_PORT),
+  });
+}
 
 export default pool;
